92bb99d82c3c02a998f4c8e58a82fe466e30b159
grubobeats <vladan.paunovic.bg@gmail.com>
2016-03-27 20:21:18 +0300

    Begininng

---
+++ frontend/views/site/index.php
@@ -0,0 +1,54 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome to iManage</h1>
+
+        <p class="lead">#1 tool for construction companies.</p>
+        <br>
+        <p><a class="btn btn-lg btn-success" href="index.php?r=site%2Fsignup">Open your account</a></p>
+        <p class="text-muted">It will take you only one minute to register and than you can start using this.</p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Who need this?</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=site%2Fabout">About us &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Why you need this?</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=site%2Fcontact">Contact us &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>How much it costs?</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Pricelist &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
