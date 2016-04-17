8246515b67c939dc8563b514369f416ccd894f00
grubobeats <vladan.paunovic.bg@gmail.com>
2016-04-17 20:52:40 +0300

    working

--- frontend/views/construction-diary/view.php
+++ frontend/views/construction-diary/view.php
@@ -14,76 +14,87 @@ $this->params['breadcrumbs'][] = ['label' => 'Construction Diaries', 'url' => ['
 $this->params['breadcrumbs'][] = $this->title;
 
 $globals = new Globals();
+
+
+$cs_company = $globals->getFromDB("csite_company" , "construction_site", "csite_id", $model->csite_id);
+$cs_investitor = $globals->getFromDB("csite_investitor" , "construction_site", "csite_id", $model->csite_id);
+$cs_address = $globals->getFromDB("csite_address" , "construction_site", "csite_id", $model->csite_id) . ", " . $globals->getFromDB("csite_city" , "construction_site", "csite_id", $model->csite_id) . " (" . $globals->getFromDB("csite_country" , "construction_site", "csite_id", $model->csite_id) . ")";
+
+
+
+
+
 ?>
 <div class="construction-diary-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+            <p>
+                <?= Html::a('Update', ['update', 'id' => $model->csdiary_id], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a('Delete', ['delete', 'id' => $model->csdiary_id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => 'Are you sure you want to delete this item?',
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </p>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->csdiary_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->csdiary_id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="row">
+        <div class="col-sm-10">
+            <table class="table table-striped table-bordered detail-view">
+                <tbody>
+                    <tr>
+                        <th colspan="4" width="100%">Day: <?= $model->extra3 ?></th>
+                    </tr>
+                    <tr>
+                        <td width="20%">Date:</td><td width="30%"><?= $model->date ?></td>
+                        <td width="20%">Weather:</td><td width="30%"><?= $model->weather . ", " . $model->temperature . " &#8451;" ?></td>
+                    </tr>
 
-    <?= DetailView::widget([
-        'model' => $model,
+                    <tr>
+                        <th width="20%">Construction site:</th>
+                        <th width="30%" colspan="3" ><?= $model->csite_name ?></th>
+                        
+                    </tr>
+                    <tr>
+                        <td width="20%">Investitor:</td>
+                        <td width="30%"><?= $cs_investitor ?></td>
+                        <td width="20%">Company:</td>
+                        <td><?= $cs_company ?></td>
+                    </tr>
 
-        'attributes' => [
-            'csite_name',
-            [
-                'attribute'=>'Company',
-                'value'=> $globals->getFromDB("csite_company" , "construction_site", "csite_id", $model->csite_id),
-            ],
-            [
-                'attribute'=>'Investitor',
-                'value'=> $globals->getFromDB("csite_investitor" , "construction_site", "csite_id", $model->csite_id),
-            ],
-            [
-                'attribute'=>'City',
-                'value'=> $globals->getFromDB("csite_city" , "construction_site", "csite_id", $model->csite_id),
-            ],
-            [
-                'attribute'=>'Address',
-                'value'=> $globals->getFromDB("csite_address" , "construction_site", "csite_id", $model->csite_id),
-            ],
-            [
-                'attribute'=>'Country',
-                'value'=> $globals->getFromDB("csite_country" , "construction_site", "csite_id", $model->csite_id),
-            ],
-            'extra3',
-            'date:date',
-            'weather',
-            [
-                'attribute'=>'Temperature',
-                'value'=> $model->temperature . " degrees",
-            ],
-            'description:ntext',
-            'issues:ntext',
-            'workers',
-            [
-                'attribute'=>'Started at time',
-                'value'=> $model->extra1 . " h",
-            ],
-            [
-                'attribute'=>'Finished at time',
-                'value'=> $model->extra2 . " h",
-            ],
-            
-        ],
+                    <tr>
+                        <td width="20%">Working hours:</td>
+                        <td width="30%"><?= $model->extra1 . " h - " . $model->extra2 . " h (" . ($model->extra2 - $model->extra1) . " working hours)" ?></td>
+                        <td width="20%">Workers:</td>
+                        <td><?= $model->workers ?></td>
+                    </tr>
+                    <tr>
+                        <th width="20%">Description:</th>
+                        <td colspan="3"><?= $model->description ?></td>
+                    </tr>
+                    <tr>
+                        <th width="20%">Issues:</th>
+                        <td colspan="3"><?= $model->issues ?></td>
+                    </tr>
+                </tbody>
+            </table>
 
-    ]) ?>
-
-    <?php
-        foreach (explode(',', $model->image) as $row)
-        {
-            echo Html::img(Yii::getAlias('@web') . '/' . $row, [ 'id' => $row, 'alt' => 'This is alt','width' => '150', 'style' => 'margin:20px; ', 'class' => 'text-center']);
-        }
-    ?>
-
-
+            <table class="table table-striped table-bordered detail-view">
+                <tbody>
+                    <?php
+                        foreach (explode(',', $model->image) as $row)
+                        {
+                            echo Html::a( "<tr><td align='center'><img src='" . $row . "' class='image'></td></tr>" , "@web/" . $row , [ 
+                                'alt'   => 'Construction diary 2016',
+                                'target'   => '_blank',
+                                ] );
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-sm-2">
+            <img src="http://demo.joomshaper.com/2012/neo/images/stories/demo/vertical-banner.jpg">
+        </div>
+    </div>
 </div>
